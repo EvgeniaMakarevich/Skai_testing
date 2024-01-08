753d35770592a8211a6a9d5f7082e7beb8091cae
EvgeniaMakarevich <evgenia.al.makarevich@gmail.com>
2024-01-08 18:42:12 +0400

    Feat:update project

--- Tests/test_pardot/test_reports_pardot.py
+++ Tests/test_pardot/test_reports_pardot.py
@@ -10,24 +10,29 @@ class TestReportsPages():
     def test_ai_report(self, get_pardot):
         driver = get_pardot
         ai_report = PardotAi(driver, Urls.pardot_ai_form_handler)
+        driver.set_window_size(1920, 1080)
         ai_report.compare_data_ai(driver)
 
     def test_amazon_report(self, get_pardot):
         driver = get_pardot
         amazon_report = PardotAmazon(driver, Urls.pardot_amazon_form_handler)
+        driver.set_window_size(1920, 1080)
         amazon_report.compare_data_amazon_playbook(driver)
 
     def test_apple_ads_report(self, get_pardot):
         driver = get_pardot
         apple_ads_report = PardotAppleAds(driver, Urls.pardot_apple_ads_form_handler)
+        driver.set_window_size(1920, 1080)
         apple_ads_report.compare_data_apple_ads(driver)
 
     def test_hidden_cost_report(self, get_pardot):
         driver = get_pardot
         hidden_cost_report = PardotHiddenCost(driver, Urls.pardot_hidden_cost_handler)
+        driver.set_window_size(1920, 1080)
         hidden_cost_report.compare_data_hidden_cost(driver)
 
     def test_prime_day_report(self, get_pardot):
         driver = get_pardot
         prime_day_report = PardotPrimeDay(driver, Urls.pardot_prime_day_form_handler)
+        driver.set_window_size(1920, 1080)
         prime_day_report.compare_data_prime_day(driver)
--- Tests/test_pardot/test_sem_pardot.py
+++ Tests/test_pardot/test_sem_pardot.py
@@ -9,22 +9,26 @@ class TestSemPages():
     def test_paid_social(self, get_pardot):
         driver = get_pardot
         paid_social = PardotPaidSocial(driver, Contact_pardot.url_contact)
+        driver.set_window_size(1920, 1080)
         paid_social.compare_data_paid_social(driver)
 
     def test_paid_search(self, get_pardot):
         driver = get_pardot
         paid_social = PardotPaidSearch(driver, Contact_pardot.url_contact)
+        driver.set_window_size(1920, 1080)
         paid_social.compare_data_paid_search(driver)
 
 
     def test_retail_solution(self, get_pardot):
         driver = get_pardot
         retail_solution = PardotRetailSolution(driver, Contact_pardot.url_contact)
+        driver.set_window_size(1920, 1080)
         retail_solution.compare_data_retail_solution(driver)
 
 
     def test_amazon_ads(self, get_pardot):
         driver = get_pardot
         amazon_ads = PardotAmazonAds(driver, Contact_pardot.url_contact)
+        driver.set_window_size(1920, 1080)
         amazon_ads.compare_data_amazon_ads(driver)
 
