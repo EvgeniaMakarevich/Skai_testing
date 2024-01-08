073d1048e7702cb5231b6cad6706a9be886a2265
EvgeniaMakarevich <evgenia.al.makarevich@gmail.com>
2024-01-08 21:10:32 +0400

    Update pardot_data.py

--- Pages/Forms/contact_page.py
+++ Pages/Forms/contact_page.py
@@ -37,6 +37,15 @@ class ContactPageMain(ContactPage):
                        # Fields_locators.selected_option_how_heard)
 
         entered_data = self.get_entered_data()
+        with open('entered_data_contact.json', 'w') as file:
+            json.dump(entered_data, file)
+
+        current_directory = os.path.dirname(os.path.abspath(__file__))
+        destination_directory = os.path.join(current_directory, Json_path.contact_page_main)
+        shutil.move('entered_data_contact.json', destination_directory)
+
+
+
         # # current_directory = os.getcwd()
         #
         # # entered_data_path = os.path.join(current_directory, 'entered_data_contact.json')
@@ -75,13 +84,7 @@ class ContactPageMain(ContactPage):
         # # Копируем файл с данными в директорию назначения
         # shutil.copy(source_file_path, destination_file_path)
 
-        destination_directory = '/home/runner/work/Skai_testing/Skai_testing/Pages/json/'
 
-        # Определяем полный путь к файлу в директории назначения
-        destination_file_path = os.path.join(destination_directory, 'entered_data_contact.json')
-
-        # Копируем файл с данными в директорию назначения
-        shutil.copy('entered_data_contact.json', destination_file_path)
 
 
 
