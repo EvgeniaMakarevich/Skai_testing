68223f4ea80e7a1a8e899fb44439b0b813538bf4
EvgeniaMakarevich <150285331+EvgeniaMakarevich@users.noreply.github.com>
2024-01-08 14:43:10 +0400

    Update pardot_data.py

--- Tests/data/pardot_data.py
+++ Tests/data/pardot_data.py
@@ -2,7 +2,7 @@ class Pardot_data:
       pardot_main = 'https://kenshoo.my.salesforce.com/?ec=302&startURL=%2F&display=page'
       username_data = 'alex.molchan@dizzain.com'
       password_data = 'Dizzain2024)(!'
-      verification_code= ('523431')
+      verification_code= ('199292')
       log_in_page = 'https://pi.pardot.com/home/index'
       form_handler_page = 'https://pi.pardot.com/formHandler'
 
