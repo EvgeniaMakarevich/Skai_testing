7e9589736470924003dde7bface282272ed07204
EvgeniaMakarevich <evgenia.al.makarevich@gmail.com>
2024-01-09 17:45:13 +0400

    Update pardot_data.py

--- Tests/data/pardot_data.py
+++ Tests/data/pardot_data.py
@@ -2,7 +2,7 @@ class Pardot_data:
       pardot_main = 'https://kenshoo.my.salesforce.com/?ec=302&startURL=%2F&display=page'
       username_data = 'alex.molchan@dizzain.com'
       password_data = 'Dizzain2024)(!'
-      verification_code= ('328462')
+      verification_code= ('792410')
       log_in_page = 'https://pi.pardot.com/home/index'
       form_handler_page = 'https://pi.pardot.com/formHandler'
 
