fb156f96790f4543bc07ef3b8f737f62720987f6
EvgeniaMakarevich <150285331+EvgeniaMakarevich@users.noreply.github.com>
2024-02-05 12:34:03 +0400

    Update pardot_data.py

--- Tests/data/pardot_data.py
+++ Tests/data/pardot_data.py
@@ -1,7 +1,8 @@
+import os
 class Pardot_data:
       pardot_main = 'https://kenshoo.my.salesforce.com/?ec=302&startURL=%2F&display=page'
-      username_data = {{USERNAME_DATA}}
-      password_data = {{PASSWORD_DATA}}
+      username_data = os.getenv("USERNAME_DATA")
+      password_data = os.getenv("PASSWORD_DATA")
       verification_code= ('673129')
       log_in_page = 'https://pi.pardot.com/home/index'
       form_handler_page = 'https://pi.pardot.com/formHandler'
